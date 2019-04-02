b8373f6b0aa0c7cfe17dbc22c67e8c8cd8d76665
Robbozinoz <Robbozinoz>
2019-04-02 15:56:47 +0800

    Navbar, footer, products sharing and site structure

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).foundation()
+
+//Navbar controls
+var prev = 0;
+var $window = $(window);
+var nav = $('.scrollhide-nav');
+
+$window.on('scroll', function () {
+    var scrollTop = $window.scrollTop();
+    nav.toggleClass('hidden', scrollTop > prev);
+    prev = scrollTop;
+});
